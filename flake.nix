816091fc786e4563e29178bbb5d651b9506ae8ef
Craig Barber <craigb@mojotech.com>
2024-03-22 18:12:31 -0400

    chore: bump to cargo-leptos 0.2.16

--- flake.nix
+++ flake.nix
@@ -20,7 +20,22 @@
           };
           rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
-          buildInputs = with pkgs; [ rust-analyzer cargo-leptos openssl mold clang ];
+          cargoLeptos = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "cargo-leptos";
+            version = "0.2.16";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "leptos-rs";
+              repo = pname;
+              rev = "07db33b6dc1f5e20521b59744f3b4fea361250f0";
+              hash = "sha256-yDBVo3GarKvcuMX7mxdxx0SJSJ5VOX3bUx6XmYxLfq4=";
+            };
+
+            cargoHash = "sha256-DZbZ3SHGWvje0gEqlx2mdLvCR4U3Xzkp8gS9FIbxW6g=";
+
+            doCheck = false;
+          };
+          buildInputs = with pkgs; [ rust-analyzer cargoLeptos openssl mold clang ];
         in
         with pkgs;
         {
